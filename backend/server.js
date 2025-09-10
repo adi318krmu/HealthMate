0bd8ef531196241d9f21e37935345489dddb5330
@A.D. <157805870+adi318krmu@users.noreply.github.com>
2025-09-10 13:05:48 +0530

    server update

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,18 @@ const express= require('express')
 const app= express()
 const dotenv=require('dotenv')
 dotenv.config();
-const port = process.env.PORT || 3000
+const cors = require('cors')
 
+const port = process.env.PORT || 3000
+app.use(express.json())
+app.use(cors())
+
+const authRoute = require('./routes/authRoute')
+
+// user authentication 
+app.post('/api', authRoute)
+
+// for checking 
 app.get("/",(req, res)=>{
     res.send("Chl rha hai bhai")
 })
