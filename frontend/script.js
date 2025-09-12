eab4d16b8e83305ed693d4e557c0823f7cc1f796
Danish <danish123@gmail.com>
2025-09-12 12:07:39 +0530

    update

---
+++ frontend/script.js
@@ -0,0 +1,9 @@
+function showSignup() {
+      document.getElementById("signin").classList.remove("active");
+      document.getElementById("signup").classList.add("active");
+    }
+
+    function showSignin() {
+      document.getElementById("signup").classList.remove("active");
+      document.getElementById("signin").classList.add("active");
+}
